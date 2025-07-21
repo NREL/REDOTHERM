4494e2497f9be891f7253f3028ab81f9c0096cd2
alidor-nrel <alon.lidor@nrel.gov>
2025-07-20 19:00:09 -0600

    Added description to the optimization wrapper function

--- functions/OptimizeRedoxWrapper.m
+++ functions/OptimizeRedoxWrapper.m
@@ -1,5 +1,7 @@
 function eta = OptimizeRedoxWrapper(X,S)
-% 
+% This function is a wrapper for the optimization capability of REDOTHERM
+% that runs the "Analyze_Redox_Cycle" function and only passes as an output
+% the cycle efficiency (since this is the optimization variable)
 Sol = Analyze_Redox_Cycle(X,S);
 eta = -Sol.eta;
 end
